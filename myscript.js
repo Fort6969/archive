cd814355c047a6ac8984870d0501ac9682baa3d4
marsianermars <124309106+marsianermars@users.noreply.github.com>
2023-02-03 05:12:22 +0100

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,28 @@
+const currentImage = document.getElementById("currentImage");
+const prevButton = document.getElementById("prevButton");
+const nextButton = document.getElementById("nextButton");
+
+const images = [
+  "SKETCH2023001.png",
+  "SKETCH2023002.png",
+  "SKETCH2023003.png",
+  // Add more image URLs here
+];
+
+let currentIndex = 0;
+
+prevButton.addEventListener("click", function() {
+  currentIndex = currentIndex - 1;
+  if (currentIndex < 0) {
+    currentIndex = images.length - 1;
+  }
+  currentImage.src = images[currentIndex];
+});
+
+nextButton.addEventListener("click", function() {
+  currentIndex = currentIndex + 1;
+  if (currentIndex >= images.length) {
+    currentIndex = 0;
+  }
+  currentImage.src = images[currentIndex];
+});
